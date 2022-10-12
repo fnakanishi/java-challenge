22fed596885e5e359f0f668325f0c1042062cd5e
Carlos Godinho <carlos@codego.dev>
2022-10-12 17:30:35 +0100

    Adding DB Unique keys

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,12 +2,13 @@ CREATE TABLE IF NOT EXISTS tb_user (
     `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
     `username` VARCHAR(50) NOT NULL,
     `password` VARCHAR(100) NOT NULL,
-    `created_at` TIMESTAMP DEFAULT now()
+    CONSTRAINT `user_uk` UNIQUE (`username`)
 );
 
 CREATE TABLE IF NOT EXISTS tb_user_role (
     `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
     `user_id` INTEGER,
     `role_name` VARCHAR(20),
-    CONSTRAINT `user_role_fk` FOREIGN KEY (`user_id`) REFERENCES tb_user (`id`)
+    CONSTRAINT `user_role_fk` FOREIGN KEY (`user_id`) REFERENCES tb_user (`id`),
+    CONSTRAINT `user_role_uk` UNIQUE (`user_id`, `role_name`)
 );
