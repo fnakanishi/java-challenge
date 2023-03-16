5928cc20303b3a947f24ec3c3b3ebd594daeb289
Carlos Godinho <carlos@codego.dev>
2023-03-16 14:09:07 +0000

    Refactoring MovieService Unit Tests

--- src/test/java/com/javachallenge/basico/service/MovieServiceTest.java
+++ src/test/java/com/javachallenge/basico/service/MovieServiceTest.java
@@ -7,17 +7,14 @@ import com.javachallenge.basico.entity.User;
 import com.javachallenge.basico.repository.MovieRepository;
 import com.javachallenge.basico.security.service.UserDetailsImpl;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Pageable;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.*;
 
 import static org.mockito.Mockito.*;
@@ -36,31 +33,13 @@ class MovieServiceTest {
     @Mock
     private MovieClient movieClient;
 
-    private Movie movieToReturnFromRepository;
+    private Movie testMovie;
 
-    @BeforeEach
-    void setup() throws Exception {
-        createMovie();
-    }
-
-    private void createMovie() throws ParseException {
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        movieToReturnFromRepository = new Movie();
-        movieToReturnFromRepository.setId("3");
-        movieToReturnFromRepository.setTitle("The Godfather");
-        movieToReturnFromRepository.setFullTitle("The Godfather (1972)");
-        movieToReturnFromRepository.setReleaseDate(formatter.parse("1972-03-24"));
-        movieToReturnFromRepository.setRuntime("2h 55min");
-        movieToReturnFromRepository.setPlot("The aging patriarch of an organized crime dynasty in postwar New York City transfers control of his clandestine empire to his reluctant youngest son.");
-        movieToReturnFromRepository.setAwards("Top rated movie #2 | Won 3 Oscars, 32 wins & 30 nominations total");
-        movieToReturnFromRepository.setDirectors("Francis Ford Coppola");
-        movieToReturnFromRepository.setWriters("Mario Puzo, Francis Ford Coppola");
-        movieToReturnFromRepository.setStars("Marlon Brando, Al Pacino, James Caan");
-        movieToReturnFromRepository.setGenres("Crime, Drama");
-        movieToReturnFromRepository.setLanguages("English, Italian, Latin");
-        movieToReturnFromRepository.setContentRating("R");
-        movieToReturnFromRepository.setImage("https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_Ratio0.7046_AL_.jpg");
-        movieToReturnFromRepository.setYear(1972);
+    private void createMovie() {
+        testMovie = new Movie();
+        testMovie.setId("3");
+        testMovie.setTitle("The Godfather");
+        testMovie.setFullTitle("The Godfather (1972)");
     }
 
     @Test
@@ -80,10 +59,11 @@ class MovieServiceTest {
 
     @Test
     void shouldReturnMovieWhenSearchingForImdbId() {
-        when(movieClient.findByImdbId(anyString(), anyString())).thenReturn(movieToReturnFromRepository);
+        createMovie();
+        when(movieClient.findByImdbId(anyString(), anyString())).thenReturn(testMovie);
 
         Movie movieResponse = service.findByImdbId("3");
-        Assertions.assertThat(movieResponse).isEqualTo(movieToReturnFromRepository);
+        Assertions.assertThat(movieResponse).isEqualTo(testMovie);
     }
 
     @Test
@@ -96,17 +76,18 @@ class MovieServiceTest {
 
     @Test
     void shouldAddMovieWhenAddingToTopMovies() {
+        createMovie();
         List<Movie> movieListToReturn = new ArrayList<Movie>();
         MovieListResource resource = new MovieListResource(movieListToReturn);
 
-        movieListToReturn.add(movieToReturnFromRepository);
-        movieListToReturn.add(movieToReturnFromRepository);
-        movieListToReturn.add(movieToReturnFromRepository);
-        movieListToReturn.add(movieToReturnFromRepository);
+        movieListToReturn.add(testMovie);
+        movieListToReturn.add(testMovie);
+        movieListToReturn.add(testMovie);
+        movieListToReturn.add(testMovie);
 
         when(movieClient.findAll(anyString())).thenReturn(resource);
         when(repository.findMovieById(anyString())).thenReturn(null);
-        when(movieClient.findByImdbId(anyString(), eq("3"))).thenReturn(movieToReturnFromRepository);
+        when(movieClient.findByImdbId(anyString(), eq("3"))).thenReturn(testMovie);
 
         service.saveTopMovies();
         verify(repository, times(4)).save(any());
@@ -114,12 +95,13 @@ class MovieServiceTest {
 
     @Test
     void findTopByFavorited() {
+        createMovie();
         List<Movie> movieListToReturn = new ArrayList<Movie>();
 
-        movieListToReturn.add(movieToReturnFromRepository);
-        movieListToReturn.add(movieToReturnFromRepository);
-        movieListToReturn.add(movieToReturnFromRepository);
-        movieListToReturn.add(movieToReturnFromRepository);
+        movieListToReturn.add(testMovie);
+        movieListToReturn.add(testMovie);
+        movieListToReturn.add(testMovie);
+        movieListToReturn.add(testMovie);
 
         when(repository.findByOrderByFavoritedDesc(Pageable.ofSize(4))).thenReturn(movieListToReturn);
         List<Movie> moviesList = service.findTopByFavorited(4);
@@ -136,36 +118,45 @@ class MovieServiceTest {
     }
 
     @Test
+    @DisplayName("Should return a movie from favorites list of another user with common favorited movie.")
     void shouldReturnAMovieWhenBestMatchIsNotEmpty() {
+        createMovie(); // Creates a full test movie at testMovie;
+
+        // Creating common movie to add to both users favorites
+        Movie commonMovie = new Movie();
+        commonMovie.setId("1");
+
+        // Creating movie list and adding commonMovie to favorites list for requested user
         Set<Movie> movieListToReturn = new HashSet<Movie>();
+        movieListToReturn.add(commonMovie);
+
+        // Creating movie list and adding commonMovie and testMovie to favorites list for the compared user
         Set<Movie> movieListToFavorite = new HashSet<Movie>();
-        Set<User> userListToFavorite = new HashSet<User>();
+        movieListToFavorite.add(commonMovie);
+        movieListToFavorite.add(testMovie);
 
-        Movie movie1 = new Movie();
-        Movie movie2 = new Movie();
-        movie1.setId("1");
-        movie2.setId("2");
-
-        movieListToReturn.add(movie1);
-        movieListToReturn.add(movie2);
-        movieListToFavorite.add(movie1);
-        movieListToFavorite.add(movie2);
-        movieListToFavorite.add(movieToReturnFromRepository);
-
+        // Creating user for favorite comparison; This user will be returned when searching for users with common favorited movies
         User user = new User("testuser", "testpassword");
         user.setId(1L);
         user.setFavorites(movieListToFavorite);
-        userListToFavorite.add(user);
-        movie1.setUsersFavorited(userListToFavorite);
-        movie2.setUsersFavorited(userListToFavorite);
-        movieToReturnFromRepository.setUsersFavorited(new HashSet<>());
 
-        when(userService.findMoviesByUsername(anyString())).thenReturn(movieListToReturn);
-        when(repository.findMovieById(anyString())).thenReturn(movie1);
+        // Creating a set of users to add to the common movie favorited list
+        Set<User> userListToFavorite = new HashSet<User>();
+        userListToFavorite.add(user);
+
+        // Adding list of favorited users to both movies
+        commonMovie.setUsersFavorited(userListToFavorite);
+        testMovie.setUsersFavorited(userListToFavorite);
+
+        // Setting response for the method called when
         when(userService.findById(anyLong())).thenReturn(user);
+        // Setting response for the favorited movies list of the requested user
+        when(userService.findMoviesByUsername(anyString())).thenReturn(movieListToReturn);
+        // Setting repository response for movie calls
+        when(repository.findMovieById(anyString())).thenReturn(commonMovie);
 
         Movie movie = service.findRandomMovie(new UserDetailsImpl(1L, "testuser", "testpassword", null));
         Assertions.assertThat(movie).isNotNull();
-        Assertions.assertThat(movie).isEqualTo(movieToReturnFromRepository);
+        Assertions.assertThat(movie).isEqualTo(testMovie);
     }
 }
